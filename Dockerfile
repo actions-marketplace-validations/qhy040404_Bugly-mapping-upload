78c90549985e9392001391e39fe0fb0df0eb4684
qhy040404 <qhy040404@163.com>
2022-08-27 09:46:32 +0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM amazoncorretto:8
+# COPY entrypoint.sh /entrypoint.sh
+# ENTRYPOINT ["/entrypoint.sh"]
+ENTRYPOINT ["entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,1 @@
+java -jar buglyqq-upload-symbol.jar -appid $appid -appkey $appkey -bundleid $bundleid -version $version -platform $platform -inputMapping $inputMapping
