d26564bcf5a4f062e35c34f1f4b112a785a086e4
qhy040404 <qhy040404@163.com>
2022-08-27 10:18:41 +0800

    Fix

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM amazoncorretto:8
 COPY entrypoint.sh /entrypoint.sh
+COPY buglyqq-upload-symbol.jar /buglyqq-upload-symbol.jar
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ls $RUNNER_WORKSPACE
-java -jar $RUNNER_WORKSPACE/buglyqq-upload-symbol.jar -appid $INPUT_APPID -appkey $INPUT_APPKEY -bundleid $INPUT_BUNDLEID -version $INPUT_VERSION -platform $INPUT_PLATFORM -inputMapping $INPUT_INPUTMAPPING
+ls $GITHUB_WORKSPACE
+java -jar /buglyqq-upload-symbol.jar -appid $INPUT_APPID -appkey $INPUT_APPKEY -bundleid $INPUT_BUNDLEID -version $INPUT_VERSION -platform $INPUT_PLATFORM -inputMapping $INPUT_INPUTMAPPING
