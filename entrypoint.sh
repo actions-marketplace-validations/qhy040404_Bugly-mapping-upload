2d14ae9ed9c1b3ea108a6e24e7d44ffffd418137
qhy040404 <qhy040404@163.com>
2022-08-27 10:21:18 +0800

    Release

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-ls $GITHUB_WORKSPACE
 java -jar /buglyqq-upload-symbol.jar -appid $INPUT_APPID -appkey $INPUT_APPKEY -bundleid $INPUT_BUNDLEID -version $INPUT_VERSION -platform $INPUT_PLATFORM -inputMapping $INPUT_INPUTMAPPING
